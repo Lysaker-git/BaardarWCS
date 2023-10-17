ef40b32d0fd4d825ca10b353c29f59c9de460605
Lysaker-git <68820814+Lysaker-git@users.noreply.github.com>
2023-10-17 21:34:45 +0200

    fresh initial commit with skeleton UI and svelte.

--- src/routes/+error.svelte
+++
@@ -1,1 +0,0 @@
-<h1>Something Wrong happened</h1>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,49 @@
-<header>
-    <nav>
-        <a href="/">Home</a>
-        <a href="/classes">Classes</a>
-        <a href="/events">Events</a>
-        <a href="/about">The Team</a>
-    </nav>
-</header>
+<script lang="ts">
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 
-<slot />
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+</script>
 
-<footer>
-    <h2>My footer</h2>
-</footer>
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">Skeleton</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://discord.gg/EXqV7W8MtY"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Discord
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://twitter.com/SkeletonUI"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Twitter
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/skeletonlabs/skeleton"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
