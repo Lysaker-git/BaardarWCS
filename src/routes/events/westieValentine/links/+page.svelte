492fb423c6520e36de7219ad8ff3b4b9e69a9c7d
Lysaker-git <68820814+Lysaker-git@users.noreply.github.com>
2024-02-08 12:58:55 +0100

    changed styling for info page to accomidate mobile

--- src/routes/events/westieValentine/links/+page.svelte
+++ src/routes/events/westieValentine/links/+page.svelte
@@ -5,67 +5,69 @@
     import arrow from '$lib/assets/arrow-right.svg';
 </script>
 
-<main class="container mx-auto grid place-items-center my-10">
-    <h1 class="h1">Westie valentines information</h1>
-    <section class="mt-10">
-        <h2 class="h2 mb-2 mx-auto">
+<main class="container grid place-content-center mx-auto">
+    <h1 class="h2 mx-auto mt-3">
+        Westie valentines information
+    </h1>
+    <section class="mx-auto my-5">
+        <h2 class="h3">
             Schedule
         </h2>
     </section>
-    <div class="card overflow-hidden shadow-md">
+    <div class="card overflow-hidden mx-2">
         <section>
-            <img src={schedule} alt="schedule to the event"/>
+            <img src={schedule} alt="schedule to the event" class=""/>
         </section>  
     </div>
-    <section>
+    <section class="my-5 mx-2 card p-3">
         <div class="">
-            <h2 class="h2 mt-10 mb-4">
+            <h2 class="h4 font-bold">
                 How do i get to Westie Valentine?
             </h2>
         </div>
-        <div class="card p-4">
-            <h2 class="h4 mb-4">There is three solid options to get to the location by subway unless you are staying at Halse Linie.</h2>
-            <p>These options are:</p>
-            <ul class="list-disc list-inside ">
-                <li class=" my-2 ">
+        <div class="">
+            <h3 class="mt-2">There is three solid options to get to the location by subway unless you are staying at Halse Linie.</h3>
+            <p class="font-bold">These options are:</p>
+            <ul class="list-disc list-inside">
+                <li class="my-2">
                     You can take subway 5 to Hasle then walk the yellow path on the map below. The path will take roughly 12 minutes to walk.
                 </li>
-                <li class=" my-2 ">
+                <li class="my-2">
                     Same as above you can take 4 or 5 to Økern subway station and then walk the Light blue path. This will roughly take you 11 minutes to walk.
                 </li>
-                <li class=" my-2 ">
+                <li class="my-2">
                     Lastly you can take subway 4 to Løren subway station and then walk the dark blue path. This route will take you roughly 18 minutes to walk.
                 </li>
             </ul>
         </div>
     </section>
-    <div class="card my-5 overflow-hidden shadow-md">
+    <div class="card overflow-hidden mx-2">
         <section>
-            <img src={overview} alt="map of westie valentine area with locations to hotel, subway and 16 restaurants" />
+            <img src={overview} alt="map of westie valentine area with locations to hotel, subway and 16 restaurants" class=""/>
         </section>
     </div>
-    <section class="w-full">
-        <h2 class="h2 mb-2">
+    <section class="my-5 mx-2">
+        <h2 class="h2 font-bold mb-4">
             Food options:
         </h2>
-        <div class="w-full">
-            <nav class="list-nav w-full">
-                <ul class="">
+        <div class="">
+            <nav class="">
+                <ul class="flex flex-wrap gap-4">
                     {#each myRestaurants as restaurant}
-                        <li class="card">
-                            <a href={restaurant.link} class="flex flex-col">
-                                <p class="block text-lg font-semibold card-header">{restaurant.id}: {restaurant.name}</p>
-                                <p class="block">Rating: {restaurant.rating}</p>
-                                <p class="block">{restaurant.details}</p>
-                                <p class="block card-footer">{restaurant.openingHours}</p>
+                        <li class="card p-4" style="flex: 1 1 20rem">
+                            <a href={restaurant.link} class="">
+                                <p class="font-bold">{restaurant.id}: {restaurant.name}</p>
+                                <p class="">Rating: {restaurant.rating}</p>
+                                <p class="break-words">{restaurant.details}</p>
+                                <p class="">{restaurant.openingHours}</p>
                             </a>
                         </li>
                     {/each}
                 </ul>
             </nav>
         </div>
-        <div>
-            <h3 class="h2 my-3">
+        <div class="card p-4 my-5">
+            <h3 class="h4">
                 Other options
             </h3>
             <p>You can also order from delivery services like foodora, wolt and more.</p>
@@ -74,8 +76,4 @@
 </main>
 
 <style>
-    .custom-flex {
-
-
-    }
 </style>
